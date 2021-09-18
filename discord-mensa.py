d6d1fd96666b3b625dbbc0b835b4ed58442f3eb9
pblan <p.blaneck@gmail.com>
2021-09-18 21:49:53 +0000

    working example

--- discord-mensa.py
+++ discord-mensa.py
@@ -1,1 +1,28 @@
-print("Hello World!")
+import Constants
+import logging
+import discord
+from openmensa import OpenMensa
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(Constants.DISCORD_BOT_TOKEN)
